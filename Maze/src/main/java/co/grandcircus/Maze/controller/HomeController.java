3f3722ff0eadf6b7e3c95481ae66a7a8dca1576a
Ryan J Montelibano <ryanjosephmonty@gmail.com>
2022-09-08 21:51:55 -0400

    changes to mazesearch jsp and HomeController

--- Maze/src/main/java/co/grandcircus/Maze/controller/HomeController.java
+++ Maze/src/main/java/co/grandcircus/Maze/controller/HomeController.java
@@ -1,5 +1,6 @@
 package co.grandcircus.Maze.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.swing.text.html.HTML;
@@ -7,38 +8,77 @@ import javax.swing.text.html.HTML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import co.grandcircus.Maze.models.Maze;
 import co.grandcircus.Maze.models.User;
+import co.grandcircus.Maze.repository.MazeRepository;
 import co.grandcircus.Maze.repository.UserRepository;
 
 @Controller
 public class HomeController {
 	@Autowired
-	private UserRepository repo;
-	
+	private UserRepository userRepo;
+	@Autowired
+	private MazeRepository mazeRepo;
+
 	@RequestMapping("/")
-	public String showIndex(@RequestParam(required=false) String message, 
-			@RequestParam(required=false) String username, @RequestParam(required=false) boolean loggedIn, Model model) {
+	public String showIndex(@RequestParam(required = false) String message,
+			@RequestParam(required = false) String username, @RequestParam(required = false) boolean loggedIn,
+			Model model) {
 		model.addAttribute("message", message);
 		model.addAttribute("username", username);
 		model.addAttribute("loggedIn", loggedIn);
 		return "index";
 	}
+
 	@RequestMapping("/usercreatemaze")
-	public String showCreateMaze(@RequestParam(required=false) String username, @RequestParam(required=false) boolean loggedIn, Model model) {
+	public String showCreateMaze(@RequestParam(required = false) String username,
+			@RequestParam(required = false) boolean loggedIn, Model model) {
 		model.addAttribute("username", username);
 		model.addAttribute("loggedIn", loggedIn);
 		return "createmaze";
 	}
+
 	@RequestMapping("/searchforamaze")
-	public String showMazeSearch(@RequestParam(required=false) String username, @RequestParam(required=false) boolean loggedIn, Model model) {
+	public String showMazeSearch(@RequestParam(required = false) String username,
+			@RequestParam(required = false) boolean loggedIn, Model model) {
 		model.addAttribute("username", username);
 		model.addAttribute("loggedIn", loggedIn);
+		List<Maze> allMazes = mazeRepo.findAll();
+		model.addAttribute("allMazes", allMazes);
 		return "mazesearch";
 	}
+
+	@RequestMapping("/searchresults")
+	public String showSearchResults(@ModelAttribute("searchTerm") String searchTerm, @RequestParam(required=false) String username, @RequestParam(required=false) boolean loggedIn, Model model) {
+		model.addAttribute("username", username);
+		model.addAttribute("loggedIn", loggedIn);
+		List<Maze> allMazes = mazeRepo.findAll();
+		List<Maze> searchedMazes;
+		model.addAttribute("allMazes", allMazes);
+		
+		/*if (!(searchTerm.equals("")) && !(searchTerm.toLowerCase() == null)) {
+			return "mazesearch";
+		}
+		else {*/
+			for(Maze mazeSearchResult : allMazes) {
+				if(mazeSearchResult.getAuthorName().equals(searchTerm)) {
+				model.addAttribute("mazeSearchResult", mazeSearchResult);
+				model.addAttribute("searchSymbolMaze", mazeDisplayWriter(mazeSearchResult.getTitle()));
+				}
+				
+				if(mazeSearchResult.getTitle().equals(searchTerm)) {
+				model.addAttribute("mazeSearchResult", mazeSearchResult);
+				model.addAttribute("searchSymbolMaze", mazeDisplayWriter(mazeSearchResult.getTitle()));
+				}
+			}return"mazesearch";
+		}
+	
+
 	@RequestMapping("/login")
 	public String showLogin(@RequestParam(required=false) String username, @RequestParam(required=false) boolean loggedIn, Model model) {
 		model.addAttribute("username", username);
@@ -47,9 +87,10 @@ public class HomeController {
 		model.addAttribute("message", message);
 		return "login";
 	}
+
 	@PostMapping("/login")
 	public String loginUser(@RequestParam String username, @RequestParam String password, Model model) {
-		Optional<User> optUser = repo.findByUsername(username);
+		Optional<User> optUser = userRepo.findByUsername(username);
 		if (optUser.isPresent()) {
 			if (optUser.get().getPassword().equals(hashPassword(password))) {
 				boolean loggedIn = true;
@@ -63,27 +104,30 @@ public class HomeController {
 		}
 		return "index";
 	}
+
 	@RequestMapping("/signup")
 	public String showSignUp(@RequestParam(required=false) String username, @RequestParam(required=false) boolean loggedIn, Model model) {
 		model.addAttribute("username", username);
 		model.addAttribute("loggedIn", loggedIn);
 		return "signup";
 	}
+
 	@PostMapping("/signup")
 	public String newUserSignUp(@RequestParam String username, @RequestParam String email, @RequestParam String password, Model model) {
-		Optional<User> optUser = repo.findByUsername(username);
+		Optional<User> optUser = userRepo.findByUsername(username);
 		if (optUser.isPresent()) {
 			model.addAttribute("message", "That username exists already. Login instead?");
 			return "login";
 		} else {
 			User user = new User(username, email, hashPassword(password));
-			repo.insert(user);
+			userRepo.insert(user);
 			model.addAttribute("message", "Welcome, " + username + "!");
 			model.addAttribute("username", username);
 			model.addAttribute("loggedIn", true);
 			return "index";
 		}
 	}
+
 	@RequestMapping("/userhome")
 	public String showUserHome(@RequestParam String username, Model model) {
 		boolean loggedIn = true;
@@ -91,19 +135,61 @@ public class HomeController {
 		model.addAttribute("username", username);
 		return "userhome";
 	}
+
 	@RequestMapping("/signout")
 	public String userSignout(@RequestParam String username, @RequestParam boolean loggedIn, Model model) {
 		model.addAttribute("loggedIn", !loggedIn);
 		model.addAttribute("message", "Come back soon, " + username + "!");
 		return "index";
 	}
+
 	@PostMapping("/usermazes")
 	public String userMazes(@RequestParam String username, @RequestParam boolean loggedIn, Model model) {
 		model.addAttribute("username", username);
 		model.addAttribute("loggedIn", loggedIn);
-		model.addAttribute("mazes", repo.findByUsername(username).get().getUserMazes());
+		model.addAttribute("userMazes", userRepo.findByUsername(username).get().getUserMazes());
+		model.addAttribute("userFavorites", userRepo.findByUsername(username).get().getUserFavorites());
 		return "usermazes";
 	}
+
+	@PostMapping("/addUserFavorite")
+	public String addToUserFavorites(@RequestParam String username, @RequestParam String title, @RequestParam boolean loggedIn, Model model) {
+		Optional<User> optUser = userRepo.findByUsername(username);
+		if (!optUser.get().getUserFavorites().contains(title)) {
+			userRepo.findAndPushToUserFavoritesByUsername(username, title);
+		}
+		model.addAttribute("username", username);
+		model.addAttribute("loggedIn", loggedIn);
+		model.addAttribute("title", title);
+		model.addAttribute("symbolMaze", mazeDisplayWriter(title));		
+		
+		return "displaymaze";
+	}
+
+	public String mazeDisplayWriter(String title) {
+		
+		Maze maze = mazeRepo.findByTitle(title);
+		
+		StringBuilder result = new StringBuilder(maze.getWidth() * (maze.getHeight() + 1));
+        for (int row = 0; row < maze.getHeight(); row++) {
+            for (int col = 0; col < maze.getWidth(); col++) {
+                if (maze.getMazeGrid()[row][col] == 0) { //Based on final variables, 0 generates a wall 
+                    result.append("#  ");
+                } else if (maze.getMazeGrid()[row][col] == 1) { //Based on final variables, 1 generates open space
+                    result.append("0  ");
+                } else if (maze.getMazeGrid()[row][col] == 2) { //Based on final variables, 2 generates maze start point
+                    result.append("S  ");
+                } else if (maze.getMazeGrid()[row][col] == 3) { //Based on final variables, 3 generates maze end point
+                    result.append("E  ");
+                } else {
+                    result.append('.'); //Everything else is the path
+                }
+            }
+            result.append("<br>");
+        }
+        return result.toString();
+	}
+
 	public static String hashPassword(String password) {
 		String result = "";
 		int mult = (password.length() * password.length()) % 126;
--- Maze/src/main/webapp/WEB-INF/views/mazesearch.jsp
+++ Maze/src/main/webapp/WEB-INF/views/mazesearch.jsp
@@ -7,6 +7,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Maze Search</title>
+<link href="/style.css" rel="stylesheet">
 </head>
 <body>
 	<div class="buttons">
@@ -39,6 +40,42 @@
 	</div>
 	
 	<h1>Maze Search</h1>
-
+	
+	<form method="post" action="/searchresults">
+		<input type="text" placeholder="Title or Author" name="searchTerm">
+		<button type="submit">Search</button>
+	</form>
+	
+	<ul>
+		<c:forEach var="maze" items="${allMazes}">
+			<li>
+				<form action="/displaymaze" method="POST">
+					<input type="hidden" name="username" value="${username}"/>
+					<input type="hidden" name="loggedIn" value="${loggedIn}"/>
+					<input type="hidden" name="title" value="${maze.title}"/>
+					<input type="submit" value="${maze.title}"/>
+				</form>
+			</li>
+		</c:forEach>
+	</ul>
+	
+	<h1>"${mazeSearchResult.getTitle()}"</h1>
+	<div class = "mazeDisplay">
+	${searchSymbolMaze}
+	</div>
+	
+	<table>
+         <tr>
+            <th>Maze Search Results</th>
+         </tr>
+         <c:forEach items="${searchedMazes}" var="searchmaze">
+            <tr>
+                <td>${searchedMazes.getTitle()</td>
+                <td>${searchedMazes.getTitle()</td>
+            </tr>
+         </c:forEach>
+      </table>
+	
+	
 </body>
 </html>
